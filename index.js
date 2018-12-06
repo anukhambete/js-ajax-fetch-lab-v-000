8e3f87a098c3bfb93be157be518f8bbfa6877c7c
Anu Khambete <anukhambete@gmail.com>
2018-12-06 02:38:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  const token = 'e414428b0822b1d75093336e629bb5b0a411877e';
+  return token;
+
+  // //return '';
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  //const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  //const token = getToken();
+  //debugger
+  // const postData = {
+  //   body: 'Great stuff'
+  // };
+  fetch(
+    'https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks',
+    {
+      method: 'POST',
+      headers: {
+      	"Authorization": `token ${getToken()}`
+      }
+    }
+  ).then(res => res.json())
+    .then(json => showResults(json));
 }
 
 function showResults(json) {
+  debugger
   //use this function to display the results from forking via the API
 }
 
